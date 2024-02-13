865c492d8c8ad45f11908a55de9bb7cba64e60ca
Cristian Arevalo <138172548+csarevalo@users.noreply.github.com>
2024-02-12 19:31:13 -0800

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const url = "https://public.tableau.com/views/CyclisticAnalysis2020/Story2?:lang
 const vizContainer = document.getElementById('vizContainer');
 const options = {
     hideTabs: true,
-    height: 1000,
-    width: 1200,
+    // height: 1000,
+    // width: 1200,
     onFirstInteraction: function() {
         workbook = viz.getWorkbook();
         activeSheet = workbook.getActiveSheet();
